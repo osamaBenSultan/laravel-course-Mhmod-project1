619350b64b769df7d47d61c93b8a3774e208188c
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2025-10-15 07:43:55 +0300

    Customizing Authentication

--- resources/views/theme/partials/header.blade.php
+++ resources/views/theme/partials/header.blade.php
@@ -3,7 +3,8 @@
         <nav class="navbar navbar-expand-lg navbar-light">
             <div class="container box_1620">
                 <!-- Brand and toggle get grouped for better mobile display -->
-                <a class="navbar-brand logo_h" href="{{route('theme.index')}}"><img src="{{asset('assets')}}/img/logo.png" alt=""></a>
+                <a class="navbar-brand logo_h" href="{{route('theme.index')}}"><img
+                        src="{{asset('assets')}}/img/logo.png" alt=""></a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
@@ -17,12 +18,16 @@
                         <li class="nav-item @yield('home-active')"><a class="nav-link" href="{{route('theme.index')}}">Home</a>
                         </li>
                         <li class="nav-item submenu dropdown @yield('category-active')">
-                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                               aria-haspopup="true"
                                aria-expanded="false">Categories</a>
                             <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="{{route('theme.category')}}">Food</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{route('theme.category')}}">Bussiness</a></li>
-                                <li class="nav-item"><a class="nav-link" href="{{route('theme.category')}}">Travel</a></li>
+                                <li class="nav-item"><a class="nav-link" href="{{route('theme.category')}}">Food</a>
+                                </li>
+                                <li class="nav-item"><a class="nav-link"
+                                                        href="{{route('theme.category')}}">Bussiness</a></li>
+                                <li class="nav-item"><a class="nav-link" href="{{route('theme.category')}}">Travel</a>
+                                </li>
                             </ul>
                         </li>
                         <li class="nav-item @yield('contact-active')"><a class="nav-link"
@@ -35,14 +40,28 @@
                     <!-- End - Add new blog -->
 
                     <ul class="nav navbar-nav navbar-right navbar-social">
-                        <a href="#" class="btn btn-sm btn-warning">Register / Login</a>
-                        <!-- <li class="nav-item submenu dropdown">
-                          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
-                            aria-expanded="false">Welcome User</a>
-                          <ul class="dropdown-menu">
-                            <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
-                          </ul>
-                        </li> -->
+                        @if(Auth::check())
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button"
+                                   aria-haspopup="true"
+                                   aria-expanded="false">{{Auth::user()->name}}</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="blog-details.html">My Blogs</a></li>
+
+                                    <li class="nav-item">
+                                        <form action="{{ route('logout') }}" method="POST">
+                                            @csrf
+                                            <a class="nav-link" href="javascript:;" onclick="this.closest('form').submit();">Logout</a>
+                                        </form>
+                                    </li>
+
+                                </ul>
+                            </li>
+
+                        @else
+                            <a href="{{route('register')}}" class="btn btn-sm btn-warning">Register / Login</a>
+                        @endif
+
                     </ul>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::controller(ThemeController::class)->name('theme.')->group(function () {
     Route::get('/category', 'category')->name('category');
     Route::get('/contact', 'contact')->name('contact');
     Route::get('/single-blog', 'singleBlog')->name('single-blog');
-    Route::get('/register', 'register')->name('register');
-    Route::get('/login', 'login')->name('login');
+//    Route::get('/register', 'register')->name('register');
+//    Route::get('/login', 'login')->name('login');
 //        Route::get('/os', 'index')->name('os');
 });
 
